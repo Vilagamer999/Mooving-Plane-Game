c59fa4697ae90218341ad7b670841ee2134d1195
Vilagamer999 <Vilagamer999@users.noreply.github.com>
2017-07-19 02:25:41 +0100

    a game...
    all done.. Unity game

---
+++ mooooooving plane/Assets/nextscene.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class nextscene : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		StartCoroutine (jumpToScene ());
+	}
+
+	IEnumerator jumpToScene (){
+		yield return new WaitForSeconds (5.0f);
+		SceneManager.LoadScene ("FPSController");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
